29549ae14c0ed52b954898db15feda79e33858c3
fitnr <fitnr@fakeisthenewreal>
2017-04-19 14:21:58 -0400

    remove sqlite, cartodb

--- src/cartodb_shape_geoms.sql
+++
@@ -1,7 +0,0 @@
-SELECT shapes.shape_id, ST_SetSRID(ST_MakeLine(shapes.the_geom), 4326) As the_geom
-  FROM (
-    SELECT shape_id, ST_MakePoint(shape_pt_lon, shape_pt_lat) AS the_geom
-    FROM shapes 
-    ORDER BY shape_id, shape_pt_sequence
-  ) AS shapes
-GROUP BY shapes.shape_id;
