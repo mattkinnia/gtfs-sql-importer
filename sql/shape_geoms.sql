73281266a4a01479fe1d83739db3e63349f728c4
fitnr <fitnr@fakeisthenewreal>
2017-05-12 09:46:09 -0400

    remove distinct from shape geoms query
    add commit to sql/shape_geoms.sql

--- sql/shape_geoms.sql
+++ sql/shape_geoms.sql
@@ -1,3 +1,5 @@
+SELECT MAX(feed_index) as feed_index FROM gtfs_feed_info;
+
 BEGIN;
 
 INSERT INTO gtfs_shape_geoms (feed_index, shape_id, the_geom)
@@ -18,14 +20,14 @@ GROUP BY feed_index, shape_id;
 
 INSERT INTO gtfs_stop_distances_along_shape
   (feed_index, shape_id, stop_id, pct_along_shape, dist_along_shape)
-SELECT DISTINCT
+SELECT
   feed_index,
   shape_id,
   stop_id,
   ROUND(CAST(ST_LineLocatePoint(route.the_geom, stop.the_geom) as numeric), 3) AS pct_along_shape,
-  ROUND(CAST(ST_LineLocatePoint(route.the_geom, stop.the_geom) * ST_length_spheroid(
-    route.the_geom, 'SPHEROID["WGS 84",6378137,298.257223563]'
-  ) as NUMERIC), 1) as dist_along_shape
+  ROUND(CAST(
+    ST_LineLocatePoint(route.the_geom, stop.the_geom) * ST_LengthSpheroid(route.the_geom, 'SPHEROID["WGS 84",6378137,298.257223563]')
+    as NUMERIC), 1)
 FROM
   (
     SELECT DISTINCT
@@ -36,3 +38,5 @@ FROM
   ) a
   LEFT JOIN gtfs_shape_geoms AS route USING (feed_index, shape_id)
   LEFT JOIN gtfs_stops as stop USING (feed_index, stop_id);
+
+COMMIT;
