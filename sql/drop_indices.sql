098cc5c00aac5730ecbe2ec14c243eb890e71b5f
fitnr <fitnr@fakeisthenewreal>
2017-06-01 16:30:54 -0400

    add cascades to drop indices

--- sql/drop_indices.sql
+++ sql/drop_indices.sql
@@ -16,25 +16,25 @@ ALTER TABLE gtfs_routes
 DROP INDEX gtfs_calendar_dates_dateidx;
 
 ALTER TABLE gtfs_fare_attributes
-  DROP CONSTRAINT gtfs_fare_attributes_pkey;
+  DROP CONSTRAINT gtfs_fare_attributes_pkey CASCADE;
 
 DROP INDEX gtfs_shapes_shape_key;
 
 ALTER TABLE gtfs_trips
-  DROP CONSTRAINT gtfs_trips_pkey;
+  DROP CONSTRAINT gtfs_trips_pkey CASCADE;
 DROP INDEX gtfs_trips_service_id;
 
 ALTER TABLE gtfs_stop_times
-  DROP CONSTRAINT gtfs_stop_times_pkey;
+  DROP CONSTRAINT gtfs_stop_times_pkey CASCADE;
 DROP INDEX gtfs_stop_times_key;
 
 DROP INDEX arr_time_index;
 DROP INDEX dep_time_index;
 
 ALTER TABLE gtfs_shape_geoms
-  DROP CONSTRAINT gtfs_shape_geom_pkey;
+  DROP CONSTRAINT gtfs_shape_geom_pkey CASCADE;
 
 ALTER TABLE gtfs_frequencies
-  DROP CONSTRAINT gtfs_frequencies_pkey;
+  DROP CONSTRAINT gtfs_frequencies_pkey CASCADE;
 
 COMMIT;
